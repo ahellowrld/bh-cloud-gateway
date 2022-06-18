63820b4de7137cb9ec49e6d753d97fbd33eff92d
张涛 <1187410392@qq.com>
2022-06-18 15:06:32 +0800

    Initial commit

---
+++ src/main/java/com/bohailife/bhcloudgetaway/BhCloudGetawayApplication.java
@@ -0,0 +1,13 @@
+package com.bohailife.bhcloudgetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhCloudGetawayApplication {
+	
+	public static void main (String[] args) {
+		SpringApplication.run (BhCloudGetawayApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/bohailife/bhcloudgetaway/BhCloudGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohailife.bhcloudgetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhCloudGetawayApplicationTests {
+	
+	@Test
+	void contextLoads () {
+	}
+	
+}
